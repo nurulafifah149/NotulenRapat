bbfb3737979e9a62365609c05f0b5880e7362367
nurulafifah149 <nurul.afifah141199@gmail.com>
2020-08-21 17:29:06 +0700

    ketiga

---
+++ NotulenRapatMulti/app/src/main/java/com/example/NotulenRapatMulti/util/Server.java
@@ -0,0 +1,8 @@
+package com.example.NotulenRapatMulti.util;
+
+public class Server {
+    public static final String URL = "http://192.168.100.20/multiuser/";
+    public static final String URL1 = "http://192.168.100.20/multiuser/";
+    public static final String URL2 = "http://192.168.100.20/multiuser/";
+
+}
